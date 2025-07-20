3b62e20cfc795ead5f40cddd75b965a587ea2f61
YahiaTr352 <yahiatr188@gmail.com>
2025-07-21 01:11:52 +0300

    remove limiter

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
 });
 
 // ✅ middleware التحديد والفلترة
-app.use(rateLimiterMiddleware);
+// app.use(rateLimiterMiddleware);
 
 // ✅ بعد limiter (ما يعيق التنفيذ)
 app.use(userAgentFilter);
